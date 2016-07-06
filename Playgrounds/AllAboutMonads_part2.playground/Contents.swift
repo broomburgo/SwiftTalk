f78eb039385ecd1e4d166c050a7fb7e83ae703a2
Elviro Rocca <elviro.rocca@facile.it>
2016-07-06 18:20:30 +0200

    started a possible implementation for monad transformers

--- Playgrounds/AllAboutMonads_part2.playground/Contents.swift
+++ Playgrounds/AllAboutMonads_part2.playground/Contents.swift
@@ -1,5 +1,5 @@
 /*:
-# All About Monads - part 1
+# All About Monads - part 2
 
 A more or less complete translation into Swift of the famous "All About Monads" Haskell tutorial, available [here](https://wiki.haskell.org/All_About_Monads). The translation will actually be  **opinionated**, because I'll make personal observations on what's going on that could be not in line with the original tutorial. Also, I'm going to use different examples.
 
@@ -41,6 +41,82 @@ extension Operation where A: EitherType {
 /*:
 Producing a custom method specific for the `Operation<A>` is not particularly elegant, it doesn't clearly express the combination strategy, and is **not composable**: seems ok for a single nested monad, but multiple nesting would require specific implementations for each level of nesting.
 
-Also, in some cases we might need to combine computations that are interdependent: for example, the combination strategy for
+Also, in some cases we might need to combine computations that are interdependent: for example, the combination strategy for an optional array of optionals could be that the first optional will result in `None` if the contained array is empty, or if it contains only `None` instances.
+
+Now, there's various possible approaches to the problem.
+
+If everything is wrapped into an operation, we need a way to produce functions of the wrapped value, which can be passed to the `bind` method, from functions that start from the inner value
 */
+extension Either {
+	static func liftOperation(base: A -> Operation<Either<A>>) -> Either<A> -> Operation<Either<A>> {
+		return { eitherA in
+			do {
+				return try base(eitherA.get())
+			}
+			catch let error {
+				return Operation.unit(.Left(error))
+			}
+		}
+	}
+}
+/*:
+Notice how the method is not really specific to `Operation<A>`: the only thing that's required is a `unit` static method.
+
+We can define a generic protocol for `unit`
+*/
+protocol UnitType {
+	associatedtype Wrapped
+	static func unit(value: Wrapped) -> Self
+}
+
+extension Operation: UnitType {
+	typealias Wrapped = A
+}
+
+extension Either {
+	static func lift<Wrapper: UnitType where Wrapper.Wrapped == Either>(base: A -> Wrapper) -> Either<A> -> Wrapper {
+		return { eitherA in
+			do {
+				return try base(eitherA.get())
+			}
+			catch let error {
+				return Wrapper.unit(.Left(error))
+			}
+		}
+	}
+}
+/*:
+Now we can generically lift every function that produces a wrapped `Either<A>` from a `A` into a function that actually starts from `Either<A>`, so that the wrapped can bind the functions (if bind is available).
+
+We can extend every monad we created with this strategy. We can even define a generic procotol for "liftable" types:
+*/
+protocol LiftType {
+	associatedtype Wrapped
+	static func lift<Wrapper: UnitType where Wrapper.Wrapped == Self>(base: Wrapped -> Wrapper) -> Self -> Wrapper
+}
+
+extension Either: LiftType {
+	typealias Wrapped = A
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 print("done")
